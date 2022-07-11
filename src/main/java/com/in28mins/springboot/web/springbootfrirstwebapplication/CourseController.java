88205d3b5b25a74168165ecb81752810ce0654ff
Ashish Malik <ashishmalik0718@gmail.com>
2022-07-10 20:52:21 -0400

    Checking commit

--- src/main/java/com/in28mins/springboot/web/springbootfrirstwebapplication/CourseController.java
+++ src/main/java/com/in28mins/springboot/web/springbootfrirstwebapplication/CourseController.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @RestController
 public class CourseController {
-
+     //Controller class
     //Id, Name, author
     @RequestMapping("/allCourses")
     public List<Course> getAllCourses() {
